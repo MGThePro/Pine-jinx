bbf83e2975de9bccf17808fe494c1e5515ebeda9
edisionnano <samantas5855@gmail.com>
2021-12-22 20:33:09 +0200

    Push PineJinx V2

--- pinejinx.sh
+++ pinejinx.sh
@@ -3,6 +3,22 @@
 find /tmp/pineapple/* ! -name '*.tar.gz' 2>/dev/null | sort -n -r | xargs rm -rf --
 mkdir -p /tmp/pineapple && cd /tmp/pineapple
 #Define the functions
+getoptions() {
+	if ! [ "$(command -v gamemoderun)" ]; then
+		printf "Warning:Gamemode not found!\nIf you want to use it you'll have to install it.\n"
+		printf "\e[91m$(tput bold)This means that if you choose Y you will have to install it manually yourself (sudo pacman -Syu gamemode on arch)!\e[0m\n"
+	fi
+	printf "Gamemode is a tool that improves performance on non custom kernels.\n"
+	read -p "Do you want to use it? [y/N]: " gamemode
+	read -p "Optimize Ryujinx for 1)Nvidia 2)Intel and AMD 3)None: " gpuopt
+	if [ "$gpuopt" = "2" ]; then
+		printf "MESA_NO_ERROR can give performance boosts in games like Monster Hunter Rise and Animal Crossing but potentially crash others like Splaton 2 and Mario Odyssey\n"
+		read -p "Do you want to use it? [y/N]: " mesanoerror
+	fi
+	read -p "Do you want to disable the console window? [y/N]: " console
+	arg="$arg2$arg3$arg1"
+	read -p "Do you want PineJinx to setup an alias for ryujinx? [y/N]: " alias
+}
 makealias() {
     ryualias='alias ryujinx="'$arg' GDK_BACKEND=x11 /home/'${USER}'/.local/share/Ryujinx/Ryujinx"'
     if [ -z "${SHELL##*zsh*}" ]; then
@@ -34,7 +50,7 @@ removealias() {
     fi
     sed -i '/alias ryujinx/d' $FILE
 }
-install () {
+install() {
 	jobid=$(curl -s https://ci.appveyor.com/api/projects/gdkchan/ryujinx/branch/master | grep -Po '"jobId":.*?[^\\]",' |sed  's/"jobId":"\(.*\)",/\1/' )
 	printf "Downloading $version...\n"
 	curl -L "https://ci.appveyor.com/api/buildjobs/${jobid}/artifacts/ryujinx-${version}-linux_x64.tar.gz" > ryujinx-${version}-linux_x64.tar.gz
@@ -51,41 +67,10 @@ install () {
 	curl -L "https://raw.githubusercontent.com/edisionnano/Pine-jinx/main/Ryujinx.desktop" > Ryujinx.desktop
 	curl -L "https://raw.githubusercontent.com/edisionnano/Pine-jinx/main/Ryujinx.png" > Ryujinx.png
 	curl -L "https://raw.githubusercontent.com/edisionnano/Pine-jinx/main/Ryujinx.xml" > Ryujinx.xml
-	if ! [ "$(command -v gamemoderun)" ]; then
-		printf "Warning:Gamemode not found!\nIf you want to use it you'll have to install it.\n"
-		printf "\e[91m$(tput bold)This means that if you choose Y you will have to install it manually yourself (sudo pacman -Syu gamemode on arch)!\e[0m\n"
-	fi
-	printf "Gamemode is a tool that improves performance on non custom kernels.\n"
-	read -p "Do you want to use it? [y/N]: " gamemode
-	if [ "$gamemode" = "y" ] || [ "$gamemode" = "Y" ]; then
-		arg1="gamemoderun "
-	else
-		arg1=""
-	fi
-	read -p "Optimize Ryujinx for 1)Nvidia 2)Intel and AMD 3)None: " gpuopt
-	if [ "$gpuopt" = "1" ]; then
-		arg2='env __GL_THREADED_OPTIMIZATIONS=0 __GL_SYNC_TO_VBLANK=0 '
-	elif [ "$gpuopt" = "2" ]; then
-		arg2="env AMD_DEBUG=w32ge,w32ps,w32cs,nohyperz,nofmask glsl_zero_init=true radeonsi_clamp_div_by_zero=true force_integer_tex_nearest=true mesa_glthread=false vblank_mode=0 "
-		printf "MESA_NO_ERROR can give performance boosts in games like Monster Hunter Rise and Animal Crossing but potentially crash others like Splaton 2 and Mario Odyssey\n"
-		read -p "Do you want to use it? [y/N]: " mesanoerror
-		if [ "$mesanoerror" = "y" ] || [ "$mesanoerror" = "Y" ]; then
-            arg3="MESA_NO_ERROR=1 "
-        else
-            arg3=""
-        fi
-	else
-		arg2=''
-	fi
-	read -p "Do you want to disable the console window? [y/N]: " console
-	if [ "$console" = "y" ] || [ "$console" = "Y" ]; then
-		sed -i "s/Terminal=true/Terminal=false/g" Ryujinx.desktop
-	fi
-	arg="$arg2$arg3$arg1"
 	#Desktop entries do not accept relative paths so the user's name must be in the file
 	sed -i "s/dummy/${USER}/g" Ryujinx.desktop
 	#Append any optimizations
-	sed -i "s/^Exec=/Exec=${arg}/" Ryujinx.desktop
+	sed -i "s/x11/x11 ${arg}/" Ryujinx.desktop
 	#Place desktop entry
 	mkdir -p /home/${USER}/.local/share/applications && cp Ryujinx.desktop /home/${USER}/.local/share/applications
 	#Place icon
@@ -98,7 +83,32 @@ install () {
 	update-mime-database /home/${USER}/.local/share/mime
 	#Update the application database
 	update-desktop-database /home/${USER}/.local/share/applications
-	read -p "Do you want PineJinx to setup an alias for ryujinx? [y/N]: " alias
+	if [ "$noconfirm" = "1" ]; then
+		:
+	else
+		getoptions
+	fi
+	if [ "$gamemode" = "y" ] || [ "$gamemode" = "Y" ]; then
+		arg1="gamemoderun "
+		curl -L "https://raw.githubusercontent.com/edisionnano/Pine-jinx/testing/gamemode.ini" > /home/${USER}/.config/gamemode.ini
+	else
+		arg1=""
+	fi
+	if [ "$gpuopt" = "1" ]; then
+		arg2='env __GL_THREADED_OPTIMIZATIONS=0 __GL_SYNC_TO_VBLANK=0 '
+	elif [ "$gpuopt" = "2" ]; then
+		arg2="env AMD_DEBUG=w32ge,w32ps,w32cs,nohyperz,nofmask glsl_zero_init=true radeonsi_clamp_div_by_zero=true force_integer_tex_nearest=true mesa_glthread=false vblank_mode=0 "
+		if [ "$mesanoerror" = "y" ] || [ "$mesanoerror" = "Y" ]; then
+            arg3="MESA_NO_ERROR=1 "
+        else
+            arg3=""
+        fi
+	else
+		arg2=''
+	fi
+	if [ "$console" = "y" ] || [ "$console" = "Y" ]; then
+		sed -i "s/Terminal=true/Terminal=false/g" Ryujinx.desktop
+	fi
 	if [ "$alias" = "y" ] || [ "$alias" = "Y" ]; then
 		makealias
 	else
@@ -107,25 +117,34 @@ install () {
 	printf "Installation successful, launch Ryujinx from your app launcher.\n"
 	printf "Also don't forget to show your love on Patreon at https://www.patreon.com/ryujinx\n"
 }
-uninstall () {
+uninstall() {
 	printf "Uninstalling..."
 	rm -rf /home/${USER}/.local/share/Ryujinx
 	rm -rf /home/${USER}/.local/share/mime/packages/Ryujinx.xml
 	rm -rf /home/${USER}/.local/share/applications/Ryujinx.desktop
 	rm -rf /home/${USER}/.local/share/icons/Ryujinx.png
+	rm -rf /home/${USER}/.config/gamemode.ini
 	update-mime-database /home/${USER}/.local/share/mime
 	update-desktop-database /home/${USER}/.local/share/applications
 	printf "\nUninstallation successful!\n"
 	removealias
 }
-printf "Welcome to PinEApple-Ryujinx\n"
-printf "Fetching latest version info from the slow AppVeyor api...\n"
-version=$(curl -s https://ci.appveyor.com/api/projects/gdkchan/ryujinx/branch/master | grep -Po '"version":.*?[^\\]",' | sed  's/"version":"\(.*\)",/\1/')
-printf "Latest version is: $version\n"
-printf "[1] Install it\n"
-printf "[2] Uninstall\n"
-printf "[3] Reinstall\Repair\n"
-read -p "Choose an option (or anything else to quit): " option
+clear
+if [ "$option" != "2" ]; then
+	printf "Welcome to PinEApple-Ryujinx\n"
+	printf "Fetching latest version info from the slow AppVeyor api...\n"
+	version=$(curl -s https://ci.appveyor.com/api/projects/gdkchan/ryujinx/branch/master | grep -Po '"version":.*?[^\\]",' | sed  's/"version":"\(.*\)",/\1/')
+	printf "Latest version is: $version\n"
+fi
+if [ -n "$option" ]; then
+	:
+else
+	printf "[1] Install it\n"
+	printf "[2] Uninstall\n"
+	printf "[3] Reinstall\Repair\n"
+	printf "[4] LDN version\n"
+	read -p "Choose an option (or anything else to quit): " option
+fi
 if [ "$option" = "1" ]; then
 	install
 elif [ "$option" = "2" ]; then
@@ -133,6 +152,8 @@ elif [ "$option" = "2" ]; then
 elif [ "$option" = "3" ]; then
 	uninstall
 	install
+elif [ "$option" = "4" ]; then
+	bash -c "$(curl -s https://raw.githubusercontent.com/edisionnano/Pine-jinx/LDN/pinejinx.sh)"
 else
 	:
 fi
